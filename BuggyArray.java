2969d2f6933d6fe4abc7d2750ee56a2aaff7a5cd
BugHunter-X <askzenai@gmail.com>
2024-12-27 00:40:04 +0100

    Add BuggyArray.java file

---
+++ BuggyArray.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i <= arr.length; i++) { // Bug: Off-by-one error
+            arr[i] = i * 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
